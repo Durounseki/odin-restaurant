5934080f69ff69b2f28fc978b31baeb5fc6f7b0e
Christian Esparza <cesparzajp@gmail.com>
2024-05-14 15:07:07 +0900

    Add homepage contact section

---
+++ src/components/get-in-touch.js
@@ -0,0 +1,76 @@
+function Contact(
+    details, 
+    titleText = "Get in Touch", 
+    mailIconFaClass = "fa-regular fa-envelope", 
+    contactText = "Contact Form", 
+    invitationText = "Please contact us by phone or via this form."
+){
+
+    this.details = details;
+    this.titleText = titleText;
+    this.mailIconFaClass = mailIconFaClass;
+    this.contactText = contactText;
+    this.invitationText = invitationText;
+
+    this.createContact = function(){
+
+        const contactContainer = document.createElement('div');
+        contactContainer.classList.add('contact-container');
+
+        const title = document.createElement('h2');
+        title.classList.add('get-in-touch');
+        title.textContent = this.titleText;
+
+        contactContainer.appendChild(title);
+
+        const phone = document.createElement('h1');
+        phone.classList.add('phone');
+        phone.textContent = details.phone;
+
+        contactContainer.appendChild(phone);
+
+        const businessHours = document.createElement('div');
+        businessHours.classList.add('business-hours');
+        for(let hours of this.details.businessHours){
+            businessHours.appendChild(this.createBusinessHours(hours));
+        }
+
+        contactContainer.appendChild(businessHours);
+
+        const contact = document.createElement('div');
+        contact.classList.add('contact');
+        const contactButton = document.createElement('div');
+        contactButton.classList.add('contact-button');
+        const mailIcon = document.createElement('i');
+        mailIcon.classList.add(...mailIconFaClass.split(' '));    //Allow multiple classes from Font Awesome
+        contactButton.appendChild(mailIcon);
+
+        const ContactText = document.createElement('p');
+        ContactText.textContent = this.contactText;
+        contactButton.appendChild(ContactText);
+
+        contact.appendChild(contactButton);
+
+        const invitation = document.createElement('p');
+        invitation.classList.add('invitation');
+        invitation.textContent = this.invitationText;
+
+        contact.appendChild(invitation);
+
+        contactContainer.append(contact);
+
+        return contactContainer;
+
+    }
+
+    this.createBusinessHours = function(hours){
+        const businessHour = document.createElement('div');
+        businessHour.classList.add('business-hour');
+        businessHour.textContent = hours;
+
+        return businessHour;
+    }
+
+}
+
+export { Contact };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,37 @@
+import { Contact } from '../components/get-in-touch.js';
 import { Location } from '../components/location.js';
 import logoImage from '../images/logo2.jpeg';
 
 function Home(){
-    
+
     const homeContent = document.createElement('div');
     homeContent.classList.add('page-content');
 
-    //Store Location
-    const storeDetails = {
+    //Contact details
+    const contactDetails = {
         address: "2260-2 Sobe, Yomitan Village, Nakagami District, Okinawa Prefecture",
         phone: "098-923-5963",
         homepage: "https://www.facebook.com/goldenwingschicken/",
+        businessHours: [
+            "Wed-Sat: 15:00 ~ 19:00 (Last Order: 18:45)",
+            "Sunday: 12:00 ~ 18:00 (Last Order: 17:45)",
+            "Mon-Tue: Closed" 
+        ],
         map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3574.1481999974667!2d127.73214348633479!3d26.38639930249866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e511fa1682df3f%3A0x51d44e7bb1ad26f0!2sGolden%20Wings%20Chicken!5e0!3m2!1sen!2sjp!4v1715597303678!5m2!1sen!2sjp",
     }
 
-    const location = new Location(storeDetails,logoImage);  //Instantiate Location object
+    //Contact Section
+
+    const contactWrapper =  document.createElement('div');
+    contactWrapper.classList.add('contact-wrapper');
+    const contact = new Contact(contactDetails);
+    const contactComponent = contact.createContact();
+
+    contactWrapper.appendChild(contactComponent);
+    homeContent.appendChild(contactWrapper);
+
+    //Location Section
+    const location = new Location(contactDetails,logoImage);  //Instantiate Location object
     const locationComponent = location.createLocation();
 
     homeContent.appendChild(locationComponent)
